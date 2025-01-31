cca8fed95f5d32edab996688d8feab44d16c389c
Tuan Anh Bui <mail@bui.app>
2025-02-01 00:14:27 +0100

    chore: update version to 0.2.0 in __init__.py

--- src/reaktiv/__init__.py
+++ src/reaktiv/__init__.py
@@ -4,5 +4,5 @@ Reactive signals for Python with first-class async support
 
 from .core import Signal, ComputeSignal, Effect
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["Signal", "ComputeSignal", "Effect"]

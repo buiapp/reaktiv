0e1534bf2b7a2632d01f2c799aa82a222af2d130
Tuan Anh Bui <mail@bui.app>
2025-02-03 10:08:41 +0100

    chore: bump version to 0.6.0

--- src/reaktiv/__init__.py
+++ src/reaktiv/__init__.py
@@ -4,5 +4,5 @@ Reactive signals for Python with first-class async support
 
 from .core import Signal, ComputeSignal, Effect
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __all__ = ["Signal", "ComputeSignal", "Effect"]

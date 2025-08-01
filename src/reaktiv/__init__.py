3f66841785c3670d70e58561f6f431c86b09237c
Tuan Anh Bui <mail@bui.app>
2025-08-01 10:27:51 +0200

    chore: bump version to 0.16.0

--- src/reaktiv/__init__.py
+++ src/reaktiv/__init__.py
@@ -14,7 +14,7 @@ from .core import (
 from .utils import to_async_iter
 from .operators import filter_signal, debounce_signal, throttle_signal, pairwise_signal
 
-__version__ = "0.15.1"
+__version__ = "0.16.0"
 __all__ = [
     "Signal",
     "ComputeSignal",
